7cd506b5cbbfaed9180ef84ee656c588d0844dff
Crack <1124241615@qq.com>
2019-01-31 16:19:41 +0800

    添加pycharts

--- 1.py
+++ 1.py
@@ -1,8 +1,15 @@
 # -*- coding: UTF-8 -*-
+
 from urllib import request
 from lxml import etree
+from pyecharts import Bar
+from pyecharts import Map ,Geo
 
 areas = ['hb', 'db', 'hd', 'hz', 'hn', 'xb', 'xn']
+Template_List=[]
+City_List=[]
+Min_List=[]
+
 def getTemplate(area):
 
     url="http://www.weather.com.cn/textFC/"+area+'.shtml'
@@ -10,9 +17,10 @@ def getTemplate(area):
     page_info = request.urlopen(page).read().decode('utf-8')
     html = etree.HTML(page_info)
     items = html.xpath('//div[@class="conMidtab"][1]/div')
-    for i in items:
-        getInfo(i)
-        print("-"*48,"\n")
+    getInfo(items[1])
+    # for i in items:
+    #     getInfo(i)
+    #     print("-"*48,"\n")
 def getInfo(items):
     items = items.xpath('table/tr')
     sum=0
@@ -38,10 +46,17 @@ def getInfo(items):
             Minimumtemp=item.xpath('td[7]/text()')[0]
         mat = "{:8}\t{:4}\t{:8}\t{:4}\t{:4}"
         print(mat.format(city,phenomena,winddirection,windpower,Minimumtemp))
+        City_List.append(city+"市")
+        Min_List.append(Minimumtemp)
         sum += 1
 
-for area in areas:
-    getTemplate(area)
+# for area in areas:
+#     getTemplate(area)
 
+getTemplate(areas[3])
 
-
+value=Min_List
+attr=City_List
+map=Map("Map 结合 VisualMap 示例",width=1200,height=600)
+map.add("",attr,value,maptype='湖南',is_visualmap=True,visual_text_color="#000")
+map.render("china_map.html")
